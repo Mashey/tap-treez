d1f6cb24f329e2b0729bd4c96eb1345a07a49161
Ryan Allen <rcallen89@gmail.com>
2021-01-27 11:48:17 -0600

    Add testing flag to limit API hits in iteration

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -4,9 +4,22 @@ import tap_treez.endpoints as endpoints
 
 @pytest.mark.vcr(record_mode='new_episodes')
 def test_product_endpoint():
-  product_list = endpoints.get_products()
+  product_list = endpoints.get_products(test=True)
 
-  # The length is a hard code result from the sandbox.  Merely to test the
-  # iteration over the product api endpoint
   assert isinstance(product_list, list)
-  # assert(len(product_list), 424)
+  assert(len(product_list), 200)
+
+@pytest.mark.vcr(record_mode='new_episodes')
+def test_ticket_endpoint():
+  ticket_list = endpoints.get_tickets(test=True)
+
+  assert isinstance(ticket_list, list)
+  assert(len(ticket_list) == 200)
+
+
+@pytest.mark.vcr(record_mode='new_episodes')
+def test_customer_endpoint():
+  customer_list = endpoints.get_customers(test=True)
+
+  assert isinstance(customer_list, list)
+  assert(len(customer_list) == 200)
