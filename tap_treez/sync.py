51e1bb16ffedc4e77542fe0645073b5509270ef1
Ryan Allen <rcallen89@gmail.com>
2021-02-16 15:05:06 -0600

    Fix bookmarking and replication keys

--- tap_treez/sync.py
+++ tap_treez/sync.py
@@ -3,6 +3,7 @@ import singer
 from singer import Transformer, metadata
 from tap_treez.client import TreezClient
 from tap_treez.streams import STREAMS
+from datetime import datetime, timezone
 
 LOGGER = singer.get_logger()
 
@@ -17,7 +18,7 @@ def sync(config, state, catalog):
         for stream in catalog.get_selected_streams(state):
             tap_stream_id = stream.tap_stream_id
             stream_obj = STREAMS[tap_stream_id](client, state)
-            # replication_key = stream_obj.replication_key
+            replication_key = stream_obj.replication_key
             stream_schema = stream.schema.to_dict()
             stream_metadata = metadata.to_map(stream.metadata)
 
@@ -36,14 +37,19 @@ def sync(config, state, catalog):
             for record in stream_obj.sync():
                 transformed_record = transformer.transform(
                     record, stream_schema, stream_metadata)
-                LOGGER.info(f"Writing record: {transformed_record}")
                 singer.write_record(
                     tap_stream_id,
                     transformed_record,
                 )
                 records += 1
-            state = singer.clear_bookmark(state, tap_stream_id, 'last_updated_date')
-            # singer.write_state(state, tap_stream_id, )
+            
+
+            singer.write_bookmark(state, 
+                tap_stream_id, 
+                replication_key, 
+                datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000-00:00"))
+            LOGGER.info(f"Total Records written: {records}")
+
 
     LOGGER.info(f'Records written: {records}')
     state = singer.set_currently_syncing(state, None)
